85f6982a2d616824cd83459d86b213d8ab2f1d36
Ambs04 <amberleejones2007@gmail.com>
2025-05-15 19:32:12 +0200

    set data state and error state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+  //set data state
+  const [state, setState] = useState([]);
+  //set error state
+  const [error, setError] = useState(null);
+
+  useEffect(() => {});
+}
 
 export default App;
