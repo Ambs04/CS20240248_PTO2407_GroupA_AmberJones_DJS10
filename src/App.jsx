31bdf7bff6d57a3ce283b875a72731e85d14cd78
Ambs04 <amberleejones2007@gmail.com>
2025-05-15 19:40:55 +0200

    set useEffect and fetch data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,22 @@ function App() {
   //set error state
   const [error, setError] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    //async function placed inside variable
+    const fetchData = async () => {
+      //try to fetch data
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await res.json();
+        setState(data);
+      } catch (err) {
+        //catch for if data fetch is not successful, then error message will be displayed in the console.
+        console.error("Error: Data Fetching Failed");
+      }
+    };
+    fetchData();
+    //useEffect set to only fetch data at initial page load
+  }, []);
 }
 
 export default App;
